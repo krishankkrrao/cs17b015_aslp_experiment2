ee07f820223c5b0d7c6b5720e910ddec5a32ddae
krishankkrrao <47662287+krishankkrrao@users.noreply.github.com>
2019-03-15 16:52:35 +0530

    Update lab2_1_comment.s

--- lab2_1_comment.s
+++ lab2_1_comment.s
@@ -18,49 +18,52 @@ main:
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
-	addiu	$sp,$sp,-24			; $sp=$sp+(-24) ; add immediate unsigned //each distinct instruction will be described once
-	sw	$fp,20($sp)			; Memory[$sp+20]=$fp ; store word
-	move	$fp,$sp				; $fp=$sp ; move
-	li	$2,10			# 0xa	; $2=10 ; load immediate
+	addiu	$sp,$sp,-24			# $sp=$sp+(-24) ; add immediate unsigned 
+	sw	$fp,20($sp)			# Memory[$sp+20]=$fp ; store word
+	move	$fp,$sp				# $fp=$sp ; move
+	li	$2,10			# 0xa	# $2=10 ; load immediate
 	sw	$2,4($fp)
 	li	$2,20			# 0x14
 	sw	$2,8($fp)
-	lw	$3,4($fp)			; $3=Memory[$fp+4] ; load word
+	lw	$3,4($fp)			# $3=Memory[$fp+4] ; load word
 	lw	$2,8($fp)
-	addu	$2,$3,$2			; $2=$3+$2 ; add unsigned
+	addu	$2,$3,$2			# $2=$3+$2 ; add unsigned
 	sw	$2,12($fp)
 	lw	$3,4($fp)
 	lw	$2,8($fp)
-	subu	$2,$3,$2			; $2=$3-$2 ; subtract unsigned
+	subu	$2,$3,$2			# $2=$3-$2 ; subtract unsigned
 	sw	$2,12($fp)
 	lw	$3,4($fp)
 	lw	$2,8($fp)
-	mul	$2,$3,$2			; $2=$3*$2 ; Multiply (without overflow)
-	sw	$2,12($fp)			; store word
+	mul	$2,$3,$2			# $2=$3*$2 ; Multiply (without overflow)
+	sw	$2,12($fp)			# store word
 	lw	$3,4($fp)
 	lw	$2,8($fp)
-	teq	$2,$0,7				; trap 7 if $2=$0
-	div	$0,$3,$2			; $hi=$0, $lo=$3/$2
-	mfhi	$2				; $2=hi ; move from hi
-	mflo	$2				; $2=lo ; move from lo
+	teq	$2,$0,7				# trap 7 if $2=$0
+	div	$0,$3,$2			# $hi=$0, $lo=$3/$2
+	mfhi	$2				# $2=hi ; move from hi
+	mflo	$2				# $2=lo ; move from lo
 	sw	$2,12($fp)		
 	lw	$3,4($fp)			
 	lw	$2,8($fp)			
-	and	$2,$3,$2			; $2=$3&$2 ; Bitwise and
+	and	$2,$3,$2			# $2=$3&$2 ; Bitwise and
 	sw	$2,12($fp)			
 	lw	$3,4($fp)			
 	lw	$2,8($fp)			
-	or	$2,$3,$2			; $1=$2|$3 ; Bitwise OR
+	or	$2,$3,$2			# $1=$2|$3 ; Bitwise OR
+	or	$2,$3,$2			# $1=$2|$3 ; Bitwise OR
+	or	$2,$3,$2			# $1=$2|$3 ; Bitwise OR
+	or	$2,$3,$2			# $1=$2|$3 ; Bitwise OR
 	sw	$2,12($fp)
 	lw	$2,4($fp)			
-	nor	$2,$0,$2			; $2=~($0|$2) ; Bitwise NOR
+	nor	$2,$0,$2			# $2=~($0|$2) ; Bitwise NOR
 	sw	$2,12($fp)
 	move	$2,$0				
 	move	$sp,$fp				
 	lw	$fp,20($sp)			
-	addiu	$sp,$sp,24			; $sp=$sp+24 ; add immediate unsigned
-	j	$31				; go to address $31 ; jump
-	nop					; This instruction will take up all 5 stages of pipeline
+	addiu	$sp,$sp,24			# $sp=$sp+24 ; add immediate unsigned
+	j	$31				# go to address $31 ; jump
+	nop					# This instruction will take up all 5 stages of pipeline
 
 	.set	macro
 	.set	reorder
